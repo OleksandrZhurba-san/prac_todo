bcc0af51edacf0ff2aeec032e50bb3ab0689a794
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2024-06-16 00:09:44 +0200

    feat: added basic functionality TODO: add strike text with change state of object and add remove button

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,101 @@
-const notesField = document.querySelector(".notes");
-const saveBtn = document.querySelector(".save");
-const notesGrid = document.querySelector(".notes-grid");
+// const notesField = document.querySelector(".notes");
+// const saveBtn = document.querySelector(".save");
+// const notesGrid = document.querySelector(".notes-grid");
+const form = document.querySelector("form");
+const descriptionInput = document.querySelector(".description");
+const dateInput = document.querySelector(".date");
+const addBtn = document.querySelector(".add");
+const clearBtn = document.querySelector(".clear");
+const todoList = document.querySelector(".todo-list");
 
-const notesStorage = JSON.parse(localStorage.getItem("notes")) || [];
+const tasksStorage = JSON.parse(localStorage.getItem("notes")) || [];
+// if (tasksStorage.length !== 0) {
+//   tasksStorage.forEach((task) => {
+//     createTask(task);
+//   });
+// }
+
 function generateRandomId() {
   return Math.floor(Math.random() * 88888888888888);
 }
 function setId() {
   let id = 0;
-  if (notesStorage.length === 0) {
+  if (tasksStorage.length === 0) {
     id = generateRandomId();
   } else {
-    id = notesStorage[notesStorage.length - 1].id + 1;
+    id = tasksStorage[tasksStorage.length - 1].id + 1;
   }
   console.log(id);
   return id;
 }
+
 function getLastId() {
-  return notesStorage[notesStorage.length - 1].id;
+  return tasksStorage[tasksStorage.length - 1].id;
 }
-function createNote(text, id) {
-  const note = document.createElement("p");
-  note.textContent = text;
-  note.setAttribute("data-id", id);
-  notesGrid.appendChild(note);
-  note.addEventListener("click", (event) => {
-    notesGrid.removeChild(event.target);
-    const noteIndex = notesStorage.map((e) => e.id).indexOf(id);
-    if (noteIndex != -1) {
-      notesStorage.splice(noteIndex, 1);
-      localStorage.setItem("notes", JSON.stringify(notesStorage));
-    }
-  });
-  return note;
+
+function createTask(taskObject) {
+  const liContainer = document.createElement("li");
+  const checkBox = document.createElement("input");
+  const taskDate = document.createElement("p");
+  const taskDescription = document.createElement("h3");
+  const inputContainer = document.createElement("div");
+  const contentContainer = document.createElement("div");
+
+  inputContainer.setAttribute("class", "input");
+  contentContainer.setAttribute("class", "content");
+  checkBox.setAttribute("type", "checkbox");
+  // checkBox.setAttribute("checked", taskObject.complete.toString());
+  checkBox.checked = taskObject.complete;
+  taskDescription.textContent = taskObject.description;
+  taskDate.textContent = taskObject.date;
+  liContainer.setAttribute("data-id", taskObject.id);
+  inputContainer.appendChild(checkBox);
+  contentContainer.append(taskDate, taskDescription);
+  liContainer.append(inputContainer, contentContainer);
+  todoList.append(liContainer);
+  // checkBox.addEventListener("change", (event) => {});
+
+  // note.addEventListener("click", (event) => {
+  //   notesGrid.removeChild(event.target);
+  //   const noteIndex = tasksStorage.map((e) => e.id).indexOf(id);
+  //   if (noteIndex != -1) {
+  //     tasksStorage.splice(noteIndex, 1);
+  //     localStorage.setItem("notes", JSON.stringify(tasksStorage));
+  //   }
+  // });
+  // return note;
 }
+
 document.addEventListener("DOMContentLoaded", () => {
-  if (notesStorage.length != 0) {
-    notesStorage.forEach((element) => {
-      console.log(element);
-      createNote(element.note, element.id);
+  if (tasksStorage.length != 0) {
+    tasksStorage.forEach((element) => {
+      createTask(element);
     });
   }
 
-  saveBtn.addEventListener("click", () => {
-    if (notesField.value !== "") {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (descriptionInput !== "") {
       let id = setId();
-      notesStorage.push({ id: id, note: notesField.value });
-      createNote(notesField.value, id);
-      localStorage.setItem("notes", JSON.stringify(notesStorage));
-      notesField.value = "";
+      const newTask = {
+        id: id,
+        description: descriptionInput.value,
+        date: dateInput.value,
+        complete: false,
+      };
+      createTask(newTask);
+      tasksStorage.push(newTask);
+      // createTask(notesField.value, id);
+      localStorage.setItem("notes", JSON.stringify(tasksStorage));
+      // notesField.value = "";
     }
   });
 });
+
+// const testCheckBox = document.querySelector(".checkBox");
+// const p = document.querySelector("p");
+// const contentDescription = document.querySelector("h3");
+// testCheckBox.addEventListener("change", (event) => {
+//   p.classList.toggle("checked");
+//   contentDescription.classList.toggle("checked");
+// });
